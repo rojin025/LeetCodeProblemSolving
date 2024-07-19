2a0d089bb8ade80d618747202f2e89a2409fad71
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-07-18 22:51:01 -0500

    Adding TobeOrNotToBe.

---
+++ 30DaysOfJS/ToBeOrNotToBe.ts
@@ -0,0 +1,20 @@
+type ToBeOrNotToBe = {
+  toBe: (val: any) => boolean;
+  notToBe: (val: any) => boolean;
+};
+
+function expect(val: any): ToBeOrNotToBe {
+  const throwError = (errorStr) => {
+    throw new Error(errorStr);
+  };
+
+  return {
+    toBe: (val2) => val2 === val || throwError("Not Equal"),
+    notToBe: (val2) => val2 !== val || throwError("Equal"),
+  };
+}
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
