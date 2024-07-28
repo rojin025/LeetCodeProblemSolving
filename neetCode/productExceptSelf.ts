fc831d890fc6e25cbd1a796487e310e7b2f03630
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-07-27 19:19:19 -0500

    Updating to On

--- neetCode/productExceptSelf.ts
+++ neetCode/productExceptSelf.ts
@@ -1,6 +1,29 @@
 function productExceptSelf(nums: number[]) {
-  let results: number[] = [];
   let n = nums.length;
+  let results: number[] = [];
+  let mulLeft: number[] = [];
+  let mulRight: number[] = [];
+
+  // left multiplication
+  let leftMul = 1;
+  mulLeft[0] = leftMul;
+  for (let i = 1; i < n; i++) {
+    leftMul *= nums[i];
+    mulLeft[i] = leftMul;
+  }
+  console.log(mulLeft);
+
+  let rightMul = 1;
+  mulRight[n - 1] = rightMul;
+  for (let i = n - 2; i >= 0; i--) {
+    rightMul *= nums[i];
+    mulRight[i] = rightMul;
+  }
+  console.log(mulRight);
+
+  for (let i = 0; i < n; i++) {
+    results[i] = mulLeft[i] * mulRight[i];
+  }
 
   /*
   O(n2)
