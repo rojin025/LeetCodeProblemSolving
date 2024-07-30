1b38e828383111aaeb45324f455aa2c5debb7445
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-07-30 17:31:21 -0500

    updating to TS code. Happy coding. :)

--- LeetCode/NumberOfGoodPairI3162.ts
+++ LeetCode/NumberOfGoodPairI3162.ts
@@ -1,2 +1,15 @@
 //3162. Find the Number of Good Pairs I
 //https://leetcode.com/problems/find-the-number-of-good-pairs-i/description/
+function numberOfPairs(nums1: number[], nums2: number[], k: number): number {
+  let counter: number = 0;
+  let m = nums1.length;
+  let n = nums2.length;
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < m; j++) {
+      if (nums1[i] % (nums2[j] * k) === 0) counter++;
+    }
+  }
+
+  return counter;
+}
