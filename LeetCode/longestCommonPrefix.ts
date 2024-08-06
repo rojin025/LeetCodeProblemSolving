e625c9f1e11af1a60f1827a9e3a9735d6518aa29
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-08-05 22:33:34 -0500

    Adding prefix var. Happy coding.

---
+++ LeetCode/longestCommonPrefix.ts
@@ -0,0 +1,9 @@
+function longestCommonPrefix(strs: string[]): string {
+  let prefix = "";
+
+  return prefix;
+}
+
+console.log("Longest Common Prefix.");
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
