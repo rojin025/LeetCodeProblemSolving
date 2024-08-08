02c7615f60b178eda8aa81c47d909e9542d6041e
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-08-08 13:50:41 -0500

    Solved.

--- LeetCode/chunkArray.ts
+++ LeetCode/chunkArray.ts
@@ -9,11 +9,17 @@ type Obj = Record<string, JSONValue> | Array<JSONValue>;
 
 function chunk(arr: JSONValue[], size: number): JSONValue[][] {
   let returnArray: JSONValue[][] = [];
+  let index = 0;
 
   for (let i = 0; i < Math.ceil(arr.length / size); i++) {
     let chunkArray: JSONValue[] = [];
-    for (let j = 0; j < arr.length; j++) {
-      chunkArray.push(arr[j]);
+    for (let j = 0; j < size; j++) {
+      if (index < arr.length) {
+        chunkArray.push(arr[index]);
+        index++;
+      } else {
+        break;
+      }
     }
     returnArray.push(chunkArray);
   }
@@ -21,4 +27,5 @@ function chunk(arr: JSONValue[], size: number): JSONValue[][] {
 }
 
 console.log("Running Chuck");
-console.log(chunk([1, 2, 3, 4, 5], 1));
+// console.log(chunk([1, 2, 3, 4, 5], 1));
+console.log(chunk([1, 2, 3, 4, 5], 4));
