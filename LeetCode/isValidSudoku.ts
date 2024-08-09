3ef030aee7488ff6dfcf7b4ed4625c15d43ad953
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-08-09 18:02:07 -0500

    Testing code.

--- LeetCode/isValidSudoku.ts
+++ LeetCode/isValidSudoku.ts
@@ -4,5 +4,31 @@
  */
 
 function isValidSudoku(board: string[][]): boolean {
-  return false;
+  const cols = new Map();
+  const rows = new Map();
+  const squares = new Map(); // key = (r/3) * 3 + c/3
+
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      const cell = board[row][col];
+      if (cell === ".") continue;
+      if (
+        rows.get(row)?.has(cell) ||
+        cols.get(col)?.has(cell) ||
+        squares.get(Math.floor(row / 3) * 3 + Math.floor(col / 3))?.has(cell)
+      )
+        return false;
+
+      cols.set(col, new Set(cols.get(col)).add(cell));
+      rows.set(row, new Set(rows.get(row)).add(cell));
+      squares.set(
+        Math.floor(row / 3) * 3 + Math.floor(col / 3),
+        new Set(squares.get(Math.floor(row / 3) * 3 + Math.floor(col / 3))).add(
+          cell
+        )
+      );
+    }
+  }
+
+  return true;
 }
