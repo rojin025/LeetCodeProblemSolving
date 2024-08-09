f4e9f77d023408a1b3b0c7ce29e27b1ebb2132f1
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-08-09 17:46:46 -0500

    Adding leetcode 36.ValidSudoku.

--- LeetCode/chunkArray.ts
+++ LeetCode/chunkArray.ts
@@ -29,3 +29,19 @@ function chunk(arr: JSONValue[], size: number): JSONValue[][] {
 console.log("Running Chuck");
 // console.log(chunk([1, 2, 3, 4, 5], 1));
 console.log(chunk([1, 2, 3, 4, 5], 4));
+
+/**
+ Better solution
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | Array<JSONValue>;
+
+function chunk(arr: Obj[], size: number): Obj[][] {
+    const chunks = [];
+    while (arr.length > 0) {
+        chunks.push(arr.splice(0, size))
+    }
+    return chunks;
+};
+
+ */
---
+++ LeetCode/isValidSudoku.ts
@@ -0,0 +1,8 @@
+/**
+ https://leetcode.com/problems/valid-sudoku/
+ 36. Valid Sudoku
+ */
+
+function isValidSudoku(board: string[][]): boolean {
+  return false;
+}
