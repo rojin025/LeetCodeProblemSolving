cc32b5d24064d9930fcaad772ee714b5dbc75a7d
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-08-10 11:32:12 -0500

    solved. Tested, Happy coding. :)

--- LeetCode/longestConsecutive.ts
+++ LeetCode/longestConsecutive.ts
@@ -19,3 +19,7 @@ function longestConsecutive(nums: number[]): number {
   }
   return longest;
 }
+
+console.log("Longest Consecutive Running");
+console.log(longestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]));
+console.log(longestConsecutive([100, 4, 200, 1, 3, 2]));
