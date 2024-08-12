373e923ad6d50e53ce3e18b078d43fc69e126beb
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-08-12 10:21:26 -0500

    updating 3sum. Completed.

--- LeetCode/3sum.ts
+++ LeetCode/3sum.ts
@@ -7,7 +7,31 @@ function threeSum(nums: number[]): number[][] {
   nums.sort((a, b) => a - b);
   const result: number[][] = [];
 
-  for (let i = 0; i < nums.length; i++) {}
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) break;
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    let l = i + 1;
+    let r = nums.length - 1;
+
+    while (l < r) {
+      const sum = nums[i] + nums[l] + nums[r];
+
+      if (sum > 0) {
+        r--;
+      } else if (sum < 0) {
+        l++;
+      } else {
+        result.push([nums[i], nums[l], nums[r]]);
+        l++;
+        r--;
+
+        while (l < r && nums[l] === nums[l - 1]) {
+          l++;
+        }
+      }
+    }
+  }
 
   return result;
 }
