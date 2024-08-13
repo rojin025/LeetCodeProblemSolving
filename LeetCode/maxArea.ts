58bebe9c82324a8035bdd6f3e362f7f101609a8f
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-08-13 11:53:07 -0500

    11. maxArea: solved

--- LeetCode/maxArea.ts
+++ LeetCode/maxArea.ts
@@ -1,3 +1,15 @@
 function maxArea(height: number[]): number {
-  return height[0];
+  let l = 0;
+  let r = height.length - 1;
+  let area = 0;
+
+  while (l < r) {
+    area = Math.max(Math.min(height[r], height[l]) * (r - l), area);
+    if (height[l] > height[r]) r--;
+    else l++;
+  }
+  return area;
 }
+
+console.log("Max Area.");
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
