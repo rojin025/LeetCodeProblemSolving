6bcc35a6de042cba5053ee2092c93c176495c27e
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-08-14 07:35:29 -0500

    Running Traped water. Happy coding :_

--- LeetCode/trap.ts
+++ LeetCode/trap.ts
@@ -3,5 +3,28 @@
  * @param height 
  */
 function trap(height: number[]): number {
-  return height[0];
+  if (!height || !height.length) return 0;
+
+  let l = 0;
+  let r = height.length - 1;
+  let leftMax = height[l];
+  let rightMax = height[r];
+  let trapedWater = 0;
+
+  while (l < r) {
+    if (leftMax < rightMax) {
+      l++;
+      leftMax = Math.max(leftMax, height[l]);
+      trapedWater += leftMax - height[l];
+    } else {
+      r--;
+      rightMax = Math.max(rightMax, height[r]);
+      trapedWater += rightMax - height[r];
+    }
+  }
+
+  return trapedWater;
 }
+
+console.log("Traped water running.");
+console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
