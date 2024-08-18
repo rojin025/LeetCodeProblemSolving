750981ab73bc71cdbde22dd5ca7b103ad55c2c3d
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-08-18 12:39:34 -0500

    739. DailyTemperatures added.

---
+++ LeetCode/dailyTemperatures.ts
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/daily-temperatures/
+
+function dailyTemperatures(temperatures: number[]): number[] {
+  let temps = new Array(temperatures.length).fill(0);
+
+  return temps;
+}
+
+console.log("Daily Temperatures running. Happy coding.");
+console.log(dailyTemperatures([1, 2, 3, 4, 5]));
