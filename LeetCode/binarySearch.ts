3e09247f8b8c2e677aad8a1f8480f13ee042e8c3
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-08-21 12:13:15 -0500

    704. Binary Search ; solved

--- LeetCode/binarySearch.ts
+++ LeetCode/binarySearch.ts
@@ -1,6 +1,24 @@
 function search(nums: number[], target: number): number {
+  let l = 0;
+  let r = nums.length - 1;
+
+  while (l <= r) {
+    let m = l + Math.floor((r - l) / 2);
+
+    if (nums[m] > target) {
+      r = m - 1;
+    } else if (nums[m] < target) {
+      l = m + 1;
+    } else {
+      return m;
+    }
+  }
+
   return -1;
 }
 
 console.log("Running BS.");
 console.log(search([1, 2, 3, 4, 5, 6], 1));
+console.log(search([1, 2, 3, 4, 5, 6], 3));
+console.log(search([1, 2, 3, 4, 5, 6], 6));
+console.log(search([1, 2, 3, 4, 5, 6], 10));
