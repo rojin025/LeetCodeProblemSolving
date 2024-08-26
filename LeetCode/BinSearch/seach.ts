ba24df21ef2404c35240e9092e84de651114bc18
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-08-26 09:53:41 -0500

    34.Search solved.

---
+++ LeetCode/BinSearch/seach.ts
@@ -0,0 +1,26 @@
+function search(nums: number[], target: number): number {
+  let l = 0;
+  let r = nums.length - 1;
+
+  while (l <= r) {
+    const mid = Math.floor((l + r) / 2);
+    if (target === nums[mid]) {
+      return mid;
+    }
+
+    if (nums[l] <= nums[mid]) {
+      if (target > nums[mid] || target < nums[l]) {
+        l = mid + 1;
+      } else {
+        r = mid - 1;
+      }
+    } else {
+      if (target < nums[mid] || target > nums[r]) {
+        r = mid - 1;
+      } else {
+        l = mid + 1;
+      }
+    }
+  }
+  return -1;
+}
