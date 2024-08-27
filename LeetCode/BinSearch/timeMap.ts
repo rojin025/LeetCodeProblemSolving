d2dcd47cf6ff386cee124162112b5d3ef39424c7
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-08-27 10:53:44 -0500

    Adding TimeMap.

---
+++ LeetCode/BinSearch/timeMap.ts
@@ -0,0 +1,18 @@
+class TimeMap {
+  constructor() {}
+
+  set(key: string, value: string, timestamp: number): void {}
+
+  get(key: string, timestamp: number): string {
+    return "";
+  }
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * var obj = new TimeMap()
+ * obj.set(key,value,timestamp)
+ * var param_2 = obj.get(key,timestamp)
+ */
+
+console.log("TimeMap Running.");
