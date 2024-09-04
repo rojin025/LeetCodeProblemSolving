5923be11ec4b215621fa6ebf8c60f052d2bbf77d
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-09-04 14:37:12 -0500

    updating reorderList in functional approach. Happy coding.

--- LeetCode/LinkedList/reorderList.ts
+++ LeetCode/LinkedList/reorderList.ts
@@ -14,43 +14,54 @@
  Do not return anything, modify head in-place instead.
  */
 
- type ListNode = {
-    value: number;
-    next: ListNode | null;
- }
-
- function reorderList(head: ListNode | null): void {
-    if ( !head) return;
-
-
-    return void;
-
+type ListNode = {
+  value: number;
+  next: ListNode | null;
 };
 
-function findMiddleNode (head: ListNode | null ): ListNode | null {
-    let slow = head;
-    let fast = head;
+function reorderList(head: ListNode | null): void {
+  if (!head) return;
 
-    while(fast && fast.next){
-        slow = slow.next;
-        fast = fast?.next?.next;
-    }
+  let middle = findMiddleNode(head);
+  let fastHead = reverse(middle.next);
+  middle.next = null;
 
-    return slow;
+  merge(head, fastHead);
 }
 
-function reverse(head: ListNode) : ListNode | null {
-    let previous = null;
-    let current = head;
+function findMiddleNode(head: ListNode | null): ListNode | null {
+  let slow = head;
+  let fast = head;
 
-    while(current) {
-        let next = current.next;
-        current.next = previous;
-        previous = current;
-        current = next;
-    }
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast?.next?.next;
+  }
 
-    return previous;
+  return slow;
+}
+
+function reverse(head: ListNode): ListNode | null {
+  let previous = null;
+  let current = head;
+
+  while (current) {
+    let next = current.next;
+    current.next = previous;
+    previous = current;
+    current = next;
+  }
+
+  return previous;
+}
+
+function merge(slowHead: ListNode | null, fastHead: ListNode | null) {
+  while (fastHead) {
+    let next = slowHead?.next;
+    slowHead.next = fastHead;
+    slowHead = fastHead;
+    fastHead = next;
+  }
 }
 
 /*
