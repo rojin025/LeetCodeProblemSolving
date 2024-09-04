98fbb87bde8e446fee5553a14e81c3fb9faef867
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-09-03 21:08:08 -0500

    update  findMiddleNode.

--- LeetCode/LinkedList/reorderList.ts
+++ LeetCode/LinkedList/reorderList.ts
@@ -13,6 +13,36 @@
 /**
  Do not return anything, modify head in-place instead.
  */
+
+ type ListNode = {
+    value: number;
+    next: ListNode | null;
+ }
+
  function reorderList(head: ListNode | null): void {
+    if ( !head) return;
+
+
     return void;
+
 };
+
+function findMiddleNode (head: ListNode ): ListNode {
+    let slow = head;
+    let fast = head;
+
+    while(fast && fast.next){
+        slow = slow.next;
+        fast = fast?.next?.next;
+    }
+
+    return slow;
+}
+
+/*
+type ListNode = {
+    val: number;
+    next: ListNode | null;
+  }; 
+  
+  */
