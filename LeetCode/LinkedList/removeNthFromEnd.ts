416f4ae5fac5e0476188411f1dd0384c39386651
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-09-05 16:59:12 -0500

    Solved.

--- LeetCode/LinkedList/removeNthFromEnd.ts
+++ LeetCode/LinkedList/removeNthFromEnd.ts
@@ -14,6 +14,43 @@ https://leetcode.com/problems/remove-nth-node-from-end-of-list/submissions/13804
  * }
  */
 
-function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
-  return;
+class ListNodeClass {
+  val: number;
+  next: ListNodeClass | null;
+
+  constructor(val: number, next: ListNodeClass | null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+function removeNthFromEnd(
+  head: ListNodeClass | null,
+  n: number
+): ListNodeClass | null {
+  const dummy = new ListNodeClass(0, head);
+
+  //   const dummy: ListNodeClass | null = {
+  //     value: 0,
+  //     next: head,
+  //   };
+
+  let left: ListNodeClass | null = dummy;
+  let right: ListNodeClass | null = head;
+
+  // point right at desired pointer
+  while (n > 0) {
+    right = right?.next;
+    n--;
+  }
+
+  // Solved by assigning left pointer at dummy
+  while (right !== null) {
+    right = right?.next;
+    left = left?.next;
+  }
+
+  // deleting node
+  left.next = left?.next?.next;
+  return dummy.next;
 }
