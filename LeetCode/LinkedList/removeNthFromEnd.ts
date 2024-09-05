22326449a8ca41422cb92057b98fbd619d26f533
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-09-05 15:38:40 -0500

    19. Remove Nth Node from end. Added

---
+++ LeetCode/LinkedList/removeNthFromEnd.ts
@@ -0,0 +1,19 @@
+/**
+https://leetcode.com/problems/remove-nth-node-from-end-of-list/submissions/1380410222/
+ */
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+  return;
+}
