7a398ef66af3f8c29310ca305bccc3f802bfc13b
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-09-06 11:52:31 -0500

    138. copyRandomList ; Solved. Happy Coding :)

--- LeetCode/LinkedList/copyRandomList.ts
+++ LeetCode/LinkedList/copyRandomList.ts
@@ -1,9 +1,28 @@
+/**
+ https://leetcode.com/problems/copy-list-with-random-pointer/ 
+ */
+
+/**
+ * Definition for _Node.
+ * class _Node {
+ *     val: number
+ *     next: _Node | null
+ *     random: _Node | null
+ *
+ *     constructor(val?: number, next?: _Node, random?: _Node) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *         this.random = (random===undefined ? null : random)
+ *     }
+ * }
+ */
+
 class _Node {
   val: number;
   next: _Node | null;
   random: _Node | null;
 
-  constructor(val: number, next: _Node, random: _Node) {
+  constructor(val?: number, next?: _Node, random?: _Node) {
     this.val = val ? val : 0;
     this.next = next ? next : null;
     this.random = random ? random : null;
@@ -11,5 +30,25 @@ class _Node {
 }
 
 function copyRandomList(head: _Node | null): _Node | null {
-  return null;
+  const hashMap = new Map();
+  hashMap.set(null, null);
+
+  let currentNode = head;
+  while (currentNode) {
+    const copyVal = new _Node(currentNode.val);
+    hashMap.set(currentNode, copyVal);
+
+    currentNode = currentNode.next;
+  }
+
+  currentNode = head;
+  while (currentNode) {
+    const cloneNode = hashMap.get(currentNode);
+    cloneNode.next = hashMap.get(currentNode.next);
+    cloneNode.random = hashMap.get(currentNode.random);
+
+    currentNode = currentNode.next;
+  }
+
+  return hashMap.get(head);
 }
