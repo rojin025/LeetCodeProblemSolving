c2b4659b3ee20d5f2a086e7b725fdd20df8349da
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-09-12 14:04:45 -0500

    543. DiameterOfBinaryTree; Solved.

--- LeetCode/Tree/diameterOfBinaryTree.ts
+++ LeetCode/Tree/diameterOfBinaryTree.ts
@@ -1,5 +1,22 @@
 //https://leetcode.com/problems/diameter-of-binary-tree/
 
 function diameterOfBinaryTree(root: _TreeNode | null): number {
-  return 0;
+  if (!root) return 0;
+
+  let diameter = 0;
+
+  function dfs(root: _TreeNode | null): number {
+    if (!root) return 0;
+
+    const leftDepth = dfs(root.left);
+    const rightDepth = dfs(root.right);
+
+    diameter = Math.max(diameter, leftDepth + rightDepth);
+
+    return 1 + Math.max(leftDepth, rightDepth);
+  }
+
+  dfs(root);
+
+  return diameter;
 }
