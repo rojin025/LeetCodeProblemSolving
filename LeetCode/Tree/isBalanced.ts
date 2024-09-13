d4a842b69ee3ffc3a5cc7359b06c4be862042745
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-09-13 11:52:18 -0500

    Solved.

--- LeetCode/Tree/isBalanced.ts
+++ LeetCode/Tree/isBalanced.ts
@@ -1,5 +1,22 @@
 function isBalanced(root: TreeNode | null): boolean {
-  let notBalance = true;
+  let isBalancedSubTree = false;
 
-  return notBalance;
+  function dfs(node: TreeNode | null): number {
+    if (!node) return 0;
+
+    if (!isBalancedSubTree) {
+      let leftDepth = dfs(node.left);
+      let rightDepth = dfs(node.right);
+
+      if (Math.abs(leftDepth - rightDepth) > 1) isBalancedSubTree = true;
+
+      return 1 + Math.max(leftDepth, rightDepth);
+    } else {
+      return 0;
+    }
+  }
+
+  dfs(root);
+
+  return !isBalancedSubTree;
 }
