c74720f78e6b6803a486183d5bcfee9a10be5cbe
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-09-14 16:44:48 -0500

    solved. Happy coding.

--- LeetCode/Tree/sameTree.ts
+++ LeetCode/Tree/sameTree.ts
@@ -1,3 +1,13 @@
 function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
-  return false;
+  if (!p && !q) return true;
+
+  if (!p || !q) {
+    return false;
+  }
+
+  return (
+    p.val === q.val &&
+    isSameTree(p.left, q!.left) &&
+    isSameTree(p.right, q!.right)
+  );
 }
