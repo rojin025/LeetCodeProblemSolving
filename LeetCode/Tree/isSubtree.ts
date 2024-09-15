0fc260cfd4f81e59dc5d33a84c49ba16e896118c
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-09-15 14:42:11 -0500

    Solved; Happy coding.

--- LeetCode/Tree/isSubtree.ts
+++ LeetCode/Tree/isSubtree.ts
@@ -1,1 +1,21 @@
-function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {}
+function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
+  if (!subRoot) return true;
+
+  if (!root) return false;
+
+  if (isSametree(root, subRoot)) return true;
+
+  return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+}
+
+function isSametree(node: TreeNode | null, subRoot: TreeNode | null): boolean {
+  if (!node && !subRoot) return true;
+
+  if (node && subRoot && node.val === subRoot.val)
+    return (
+      isSametree(node.left, subRoot.left) &&
+      isSametree(node.right, subRoot.right)
+    );
+
+  return false;
+}
