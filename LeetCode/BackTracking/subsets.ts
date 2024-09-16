48f86252fc2166d4f6d9c5ddce838aef52f77f56
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-09-16 11:36:43 -0500

    solved.

--- LeetCode/BackTracking/subsets.ts
+++ LeetCode/BackTracking/subsets.ts
@@ -1,5 +1,25 @@
 //https://leetcode.com/problems/subsets/
+//https://neetcode.io/problems/subsets
 
 function subsets(nums: number[]): number[][] {
-  return [];
+  const res: number[][] = [];
+  const subset: number[] = [];
+
+  function dfs(nums: number[], i: number) {
+    if (i >= nums.length) {
+      res.push([...subset]);
+      return;
+    }
+
+    // adding left node
+    subset.push(nums[i]);
+    dfs(nums, i + 1);
+
+    // doing nothing to right node--> removing right node
+    subset.pop();
+    dfs(nums, i + 1);
+  }
+  dfs(nums, 0);
+
+  return res;
 }
