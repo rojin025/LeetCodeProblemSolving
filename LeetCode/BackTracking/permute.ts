09d7b6d1366afb2aca14f3c67398756c95e1b2be
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-09-18 12:06:00 -0500

    Solved

--- LeetCode/BackTracking/permute.ts
+++ LeetCode/BackTracking/permute.ts
@@ -1,3 +1,24 @@
 //https://leetcode.com/problems/permutations/
 
-function permute(nums: number[]): number[][] {}
+function permute(nums: number[]): number[][] {
+  const res: number[][] = [];
+
+  if (nums.length === 1) {
+    res.push([nums[0]]);
+    return res;
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    const remainingNums = nums.filter((_, j) => i !== j);
+
+    const perms = permute(remainingNums);
+
+    for (const perm of perms) {
+      perm.push(num);
+      res.push([...perm]);
+    }
+  }
+
+  return res;
+}
