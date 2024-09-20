38519c784f91bbb7b234f4e8516eafbec2352655
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-09-20 10:50:57 -0500

    40. Combination Sum 2; Solved

--- LeetCode/BackTracking/combinationSum2.ts
+++ LeetCode/BackTracking/combinationSum2.ts
@@ -3,5 +3,26 @@
 //https://leetcode.com/problems/combination-sum-ii/
 
 function combinationSum2(candidates: number[], target: number): number[][] {
-  return [];
+  let res: number[][] = [];
+  candidates.sort();
+
+  function backTack(left: number, candidate: number, combination: number[]) {
+    if (candidate === 0) return res.push([...combination]);
+
+    if (candidate < 0) return;
+
+    for (let i = left; i < candidates.length; i++) {
+      if (i > left && candidates[i] === candidates[i - 1]) continue;
+
+      combination.push(candidates[i]);
+      backTack(i + 1, candidate - candidates[i], combination);
+      combination.pop();
+    }
+  }
+
+  backTack(0, target, []);
+  return res;
 }
+
+console.log("Combination sum 2 running.");
+console.log(combinationSum2([10, 1, 2, 7, 6, 1, 5], 8));
