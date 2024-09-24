efcb53c75134bffad88b34d5d2e1f65c1981e0bd
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-09-24 11:07:05 -0500

    1046. LastStoneWeight; Added

---
+++ LeetCode/Heap/PriorityQueue/lastStoneWeight.ts
@@ -0,0 +1,6 @@
+// https://neetcode.io/problems/last-stone-weight
+// https://leetcode.com/problems/last-stone-weight/solutions/4583094/last-stone-weight-using-internal-typescript-priorityqueue-datastructure-internal-library/
+
+function lastStoneWeight(stones: number[]): number {
+  return 0;
+}
