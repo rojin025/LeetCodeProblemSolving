e8e36e8e71c30c719991863a260aab6b15da88e4
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-09-27 14:45:25 -0500

    Solving 70. ClimbStairs; Solved

--- LeetCode/DynamicProgramming/climbStairs.ts
+++ LeetCode/DynamicProgramming/climbStairs.ts
@@ -1,15 +1,20 @@
 function climbStairs(n: number): number {
+  if (n <= 3) return n;
+
   let one = 1;
   let two = 1;
   let temp = 2;
 
-  for (let i = n - 2; i >= 0; i++) {
+  for (let i = n - 2; i >= 0; i--) {
     temp = one + two;
     one = two;
     two = temp;
   }
 
-  return one;
+  return two;
 }
 
 console.log("climbStairs running");
+console.log(climbStairs(2));
+console.log(climbStairs(3));
+console.log(climbStairs(5));
