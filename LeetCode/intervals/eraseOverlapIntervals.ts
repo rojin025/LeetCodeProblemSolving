96cad69a101bd955c6dd2c790f670933da10b9fb
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-10-05 11:55:45 -0500

    Solved.

--- LeetCode/intervals/eraseOverlapIntervals.ts
+++ LeetCode/intervals/eraseOverlapIntervals.ts
@@ -1,7 +1,29 @@
-function eraseOverlapIntervals(intervals: number[][]): number {
-  const nonOverlapIntervals: number = 0;
+//https://neetcode.io/problems/non-overlapping-intervals
+//https://leetcode.com/problems/non-overlapping-intervals/description/
 
-  return nonOverlapIntervals;
+function eraseOverlapIntervals(intervals: number[][]): number {
+  let numToErase: number = 0;
+
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  console.log(intervals);
+
+  let prevEnd = intervals[0][1];
+
+  for (let i = 1; i < intervals.length; i++) {
+    const start = intervals[i][0];
+    const end = intervals[i][1];
+
+    if (start < prevEnd) {
+      numToErase++;
+      console.log("Removing", Math.max(prevEnd, end));
+      prevEnd = Math.min(prevEnd, end);
+    } else {
+      prevEnd = end;
+    }
+  }
+
+  return numToErase;
 }
 
 console.log("Running Erase Overlap Intervals.");
@@ -22,3 +44,39 @@ console.log(
     [2, 4],
   ])
 );
+
+// output = 2
+console.log(
+  eraseOverlapIntervals([
+    [1, 2],
+    [1, 2],
+    [1, 2],
+  ])
+);
+
+// output = 2
+console.log(
+  eraseOverlapIntervals([
+    [1, 100],
+    [1, 11],
+    [2, 12],
+    [11, 22],
+  ])
+);
+
+// Check it out
+function eraseOverlapIntervals2(intervals: number[][]): number {
+  intervals.sort((a, b) => a[1] - b[1]);
+  let count = 0,
+    lastEl = intervals[0][1];
+
+  for (let i = 1; i < intervals.length; i++) {
+    if (intervals[i][0] >= lastEl) {
+      lastEl = intervals[i][1];
+    } else {
+      count++;
+    }
+  }
+
+  return count;
+}
