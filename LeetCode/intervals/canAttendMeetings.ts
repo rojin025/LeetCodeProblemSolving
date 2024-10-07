1b3aa82fd81b49129648e2af1f1f2e110df62d97
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-10-07 11:50:08 -0500

    252. CanAttendMeetings. added

---
+++ LeetCode/intervals/canAttendMeetings.ts
@@ -0,0 +1,22 @@
+function canAttendMeetings(intervals: number[][]): boolean {
+  let res = false;
+
+  return res;
+}
+
+console.log("Running CanAttend Meetings.");
+
+console.log(
+  canAttendMeetings([
+    [0, 30],
+    [5, 10],
+    [15, 20],
+  ])
+);
+
+console.log(
+  canAttendMeetings([
+    [0, 3],
+    [5, 10],
+  ])
+);
