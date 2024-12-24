588066e4821cfb218b32d4b5d334379cdb544d2d
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-12-24 12:36:20 -0600

    211| Solved. :D Happy coding.

--- LeetCode/Tries/WordDectionary.ts
+++ LeetCode/Tries/WordDectionary.ts
@@ -1,9 +1,52 @@
 class WordDictionary {
-  constructor() {}
+  private root: TrieNode;
 
-  addWord(word: string): void {}
+  constructor() {
+    this.root = new TrieNode();
+  }
+
+  addWord(word: string): void {
+    let cur = this.root;
+
+    for (const c of word) {
+      if (!cur.children.has(c)) cur.children.set(c, new TrieNode());
+
+      cur = cur.children.get(c)!;
+    }
+
+    cur.endOfword = true;
+  }
 
   serach(word: string): boolean {
     return false;
   }
+
+  private dfs(word: string, curIndex: number, root: TrieNode): boolean {
+    let curNode = root;
+
+    for (let i = curIndex; i < word.length; i++) {
+      const char = word[i];
+
+      if (char === ".") {
+        // Handle all child nodes
+        for (const children of curNode.children.values()) {
+          if (this.dfs(word, i + 1, children)) return true;
+        }
+        return false;
+      } else {
+        if (!curNode.children.has(char)) return false;
+
+        curNode = curNode.children.get(char)!;
+      }
+    }
+
+    return curNode.endOfword;
+  }
 }
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * var obj = new WordDictionary()
+ * obj.addWord(word)
+ * var param_2 = obj.search(word)
+ */
