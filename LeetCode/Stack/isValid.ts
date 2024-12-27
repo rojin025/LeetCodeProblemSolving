94a2d7da9ed702a2390b74c4225b0a4819a23e5c
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-12-27 12:03:17 -0600

    ADD | 20. Valid Parentheses. Happy Coding

---
+++ LeetCode/Stack/isValid.ts
@@ -0,0 +1,35 @@
+/**
+
+Problem Link
+ https://leetcode.com/problems/valid-parentheses/
+ https://neetcode.io/problems/validate-parentheses
+
+Resources Additional links:
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/hasOwnProperty
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/in
+
+ */
+
+const bracketsClosed: { [key: string]: string } = {
+  "]": "[",
+  "}": "{",
+  ")": "(",
+};
+
+function isValid(s: string): boolean {
+  const stack: string[] = [];
+
+  for (const char of s) {
+    if (char in bracketsClosed) {
+      if (bracketsClosed[char] !== stack.pop()) return false;
+    } else {
+      stack.push(char);
+    }
+  }
+
+  return stack.length === 0;
+}
+
+console.log(isValid("()"));
+console.log(isValid("()[]{}"));
+console.log(isValid("([])"));
