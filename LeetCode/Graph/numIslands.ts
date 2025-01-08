a9bdcef4bbf99900174ac6da2c3b19feb95546ac
Rojin Bijukchhe <ro_jin25@icloud.com>
2025-01-07 21:56:37 -0600

    ADD | GRAPH

---
+++ LeetCode/Graph/numIslands.ts
@@ -0,0 +1,10 @@
+// https://neetcode.io/problems/count-number-of-islands
+// https://leetcode.com/problems/number-of-islands/
+
+function numIslands(grid: string[][]): number {
+  if (grid.length === 0) return 0;
+
+  let island = 0;
+
+  return island;
+}
