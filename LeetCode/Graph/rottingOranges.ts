9b4acb7ea60e18a4a80b370a6f4ffc110939b403
Rojin Bijukchhe <ro_jin25@icloud.com>
2025-01-10 17:06:36 -0600

    ADD | 994. Rotting Orages added. Happy coding :D

---
+++ LeetCode/Graph/rottingOranges.ts
@@ -0,0 +1,10 @@
+/**
+ URL
+    https://leetcode.com/problems/rotting-oranges/
+    https://neetcode.io/problems/rotting-fruit
+
+ * @param grid
+ */
+function orangesRotting(grid: number[][]): number {
+  return -1;
+}
